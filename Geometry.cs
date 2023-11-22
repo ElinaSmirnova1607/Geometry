41ae601e13c53ba267f9e6ad70c30bec5c875816
ElinaSmirnova1607 <147576900+ElinaSmirnova1607@users.noreply.github.com>
2023-11-22 13:38:09 +1000

    Add files via upload

---
+++ Geometry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchTask_SmirFergs_
+{
+    public class Geometry
+    {
+        public double RectangleArea(int a, int h)
+        {
+            return Math.Round(Math.PI * (a * a) * h);
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MatchTask_SmirFergs_;
+
+namespace MatchTest_SmirFergs_
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 3;
+            int h = 5;
+            int expected = 141;
+            Geometry g = new Geometry();
+            double actual = g.RectangleArea(a, h);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
